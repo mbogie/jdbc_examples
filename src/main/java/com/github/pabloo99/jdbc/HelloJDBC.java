1994aa3a9c9411d8ba7096b2ed3afbc560e96753
Paweł Mazur <pawel-mazur2@o2.pl>
2018-01-06 13:53:54 +0100

    Initial commit

---
+++ src/main/java/com/github/pabloo99/jdbc/HelloJDBC.java
@@ -0,0 +1,12 @@
+package com.github.pabloo99.jdbc;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello JDBC !");
+    }
+
+}
